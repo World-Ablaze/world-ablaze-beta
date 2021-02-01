6bb34a0500803b38550955ee9532b8d9d42bf1e6
Uncharted95 <unchartedslegacy@gmail.com>
2021-02-01 15:08:12 +0000

    amphibious changes

--- common/defines/05_defines.lua
+++ common/defines/05_defines.lua
@@ -259,7 +259,7 @@ NDefines.NResistance.RESISTANCE_ACTIVITY_CHANCE_AT_MAX_RESISTANCE = 0.200			-- s
 NDefines.NAir.LEND_LEASED_EQUIPMENT_EXPERIENCE_GAIN = 0.0							-- Value used for equipment
 
 NDefines.NAir.CAPACITY_PENALTY = 10													-- scales penalty of having overcrowded bases.
-	
+NDefines.NAir.AIR_WING_MAX_SIZE = 1600 												-- Max amount of airplanes in wing
 NDefines.NAir.MISSION_EFFICIENCY_MULT_AT_LACK_OF_FUEL = 0.05		 				-- multiplier for mission efficiency when a base lacks fuel
 
 NDefines.NAir.CLOSE_AIR_SUPPORT_EXPERIENCE_SCALE = 0.00025							-- How much the experinence gained by CAS is scaled
