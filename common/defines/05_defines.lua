c13dcda3fd81eb76d9ae7f1bbfb957efec67a574
Uncharted95 <unchartedslegacy@gmail.com>
2021-10-26 18:42:21 +0100

    Final changes for 6.4 (seek major sponsor)

--- common/defines/05_defines.lua
+++ common/defines/05_defines.lua
@@ -294,6 +294,7 @@ NDefines.NAir.CAPACITY_PENALTY = 2													-- scales penalty of having overc
 NDefines.NAir.AIR_WING_MAX_SIZE = 1600 												-- Max amount of airplanes in wing
 NDefines.NAir.MISSION_EFFICIENCY_MULT_AT_LACK_OF_FUEL = 0.05		 				-- multiplier for mission efficiency when a base lacks fuel
 
+NDefines.NAir.FIELD_EXPERIENCE_SCALE = 0.0001
 NDefines.NAir.CLOSE_AIR_SUPPORT_EXPERIENCE_SCALE = 0.00015							-- How much the experinence gained by CAS is scaled
 
 NDefines.NAir.AIR_WING_MAX_STATS_ATTACK = 10000										-- Max stats
