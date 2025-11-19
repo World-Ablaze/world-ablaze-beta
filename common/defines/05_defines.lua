dfffddb75b97eccedd28f12e3c8e7af7226620d2
Ethan156 <48657373+Ethan156@users.noreply.github.com>
2025-11-19 13:21:07 -0600

    Prevent AI spamming blockade too early

--- common/defines/05_defines.lua
+++ common/defines/05_defines.lua
@@ -30,6 +30,7 @@ NDefines.NDiplomacy.FLEET_FEAR = 0													-- Impact on troops on borders wh
 NDefines.NDiplomacy.BASE_SEND_ATTACHE_COST = 50										-- Political power cost to send attache
 
 NDefines.NDiplomacy.EMBARGO_THREAT_THRESHOLD = 50									-- Target-generated threat threshold to allow embargo (affected by modifiers)
+NDefines.NDiplomacy.NAVAL_BLOCKADE_THREAT_THRESHOLD = 50,							-- Target-generated threat threshold to allow naval blockade
 
 NDefines.NDiplomacy.LL_TO_OVERLORD_AUTONOMY_DAILY_BASE = 0.0						-- If puppet lend leases equipment to overlord of at least same tech level as they have, they gain autonomy
 NDefines.NDiplomacy.LL_TO_OVERLORD_AUTONOMY_DAILY_FACTOR = 0.0						-- If puppet lend leases equipment to overlord of at least same tech level as they have, they gain autonomy
